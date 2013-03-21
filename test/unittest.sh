cb0025f3a2346f98c864c1dcbdb73432e3df705b
Robin Cao <hcao@rbcon.com>
2013-03-21 00:40:06 -0700

    add travis ci yml file

--- test/unittest.sh
+++ test/unittest.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+cd ..
+cd app
+npm install
+cd ..
+cd test
+npm install
 TEST_FILES="$@"
 [ -z "$TEST_FILES" ] && TEST_FILES="$(find . -name '*unit-test.js')"
 ./node_modules/.bin/mocha --reporter nyan -r mootools $TEST_FILES
