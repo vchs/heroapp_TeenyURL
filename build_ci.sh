5c02f41d61f82892340eaabc48127895a2a00338
Robin Cao <hcao@rbcon.com>
2013-03-28 21:08:21 -0700

    modify travis

--- build_ci.sh
+++ build_ci.sh
@@ -5,7 +5,7 @@ npm install
 cd ..
 cd test
 npm install
-VCAP_SERVICES='{ "postgres": [{"name" : "teenyurl-postgres", "credentials" : { "database" : "teenyurl", "username" : "postgres" }}] }'
+export VCAP_SERVICES='{ "postgres": [{"name" : "teenyurl-postgres", "credentials" : { "database" : "teenyurl", "username" : "postgres" }}], "redis": [{"name": "teenyurl-redis-cache", "credentials": { "host": "localhost", "port": 6379, "password": "" } }]}'
 TEST_FILES="$@"
 [ -z "$TEST_FILES" ] && TEST_FILES="$(find . -name '*-test.js' -not -name 'Mongo*')"
 ./node_modules/.bin/mocha --reporter nyan -r mootools $TEST_FILES
