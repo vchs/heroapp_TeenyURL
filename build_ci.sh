59f0f182b8d7fb96117d80c0be39df60ba04e66b
Robin Cao <hcao@rbcon.com>
2013-03-28 20:54:46 -0700

    modify travis

--- build_ci.sh
+++ build_ci.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-
+set -x
 cd app
 npm install
 cd ..
 cd test
 npm install
+VCAP_SERVICES='{ "redis": [{"name": "teenyurl-redis-cache", "credentials": { "host": "localhost", "port": 6379, "password": "" } }]}'
 TEST_FILES="$@"
 [ -z "$TEST_FILES" ] && TEST_FILES="$(find . -name '*-test.js' -not -name 'Mongo*')"
 ./node_modules/.bin/mocha --reporter nyan -r mootools $TEST_FILES
