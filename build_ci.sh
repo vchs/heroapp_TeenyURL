071decf42fc9ac914d5d170bd4653e4cfcb0436e
Robin Cao <hcao@rbcon.com>
2013-04-01 02:35:38 -0700

    modify build_ci.sh

--- build_ci.sh
+++ build_ci.sh
@@ -6,9 +6,11 @@ cd ..
 cd test
 npm install
 export VCAP_SERVICES='{ "postgres": [{"name" : "teenyurl-postgres", "credentials" : { "database" : "teenyurl", "username" : "postgres" }}], "redis": [{"name": "teenyurl-redis-cache", "credentials": { "host": "localhost", "port": 6379, "password": "" } }]}'
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   start unit tests and integration tests   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 ./test.sh
 cd ..
 cd ui-automation
 npm install
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   start UI automation tests   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 ./test.sh
 
