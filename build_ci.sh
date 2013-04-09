bf5bd8d51c0540350c0f7751518b847c64161ce4
Robin Cao <hcao@rbcon.com>
2013-04-08 23:52:10 -0700

    exclude mongo cases

--- build_ci.sh
+++ build_ci.sh
@@ -9,7 +9,7 @@ npm install
 export VCAP_SERVICES='{ "postgres": [{"name" : "teenyurl-postgres", "credentials" : { "name" : "teenyurl", "username" : "postgres" }}], "redis": [{"name": "teenyurl-redis-cache", "credentials": { "host": "localhost", "port": 6379, "password": "" } }]}'
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   start unit tests and integration tests   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 TEST_FILES="$@"
-[ -z "$TEST_FILES" ] && TEST_FILES="$(find . -name '*-test.js')"
+[ -z "$TEST_FILES" ] && TEST_FILES="$(find . -name '*-test.js' -not -name 'Mongo*')"
 ./node_modules/.bin/mocha --reporter list -r mootools $TEST_FILES || exitcode=1
 echo $exitcode
 cd ..
